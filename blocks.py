0ea8ab5b859eb968a26c870901827bc233917643
torbin2 <torbinnet@gmail.com>
2024-08-07 17:38:29 +0200

    cube

--- blocks.py
+++ blocks.py
@@ -4,15 +4,16 @@ class Blocks:
         self.screen = screen
 
         self.blocks = {}
-        for x in range(6):
-            self.blocks[-1, x] = Block(-0.66, x)
-            self.blocks[12, x] = Block(12.33, x)
+        for x in range(10):
+            self.blocks[111, x] = Block(-0.66, x-2)
+            self.blocks[222, x] = Block(12.66, x-2)
 
         for x in range(14):
-            self.blocks[x-1, 6] = Block(x-1, 6)
-            self.blocks[x-1, 0] = Block(x-1, 0)
+            self.blocks[x, 111] = Block(x-1, 6.66)
+            self.blocks[x, 222] = Block(x-1, -0.66)
         
         self.blocks[6, 3] = Block(6, 3)
+        self.blocks[7, 3] = Block(7, 3)
     
     def render(self):
         for block in self.blocks:
--- player.py
+++ player.py
@@ -10,25 +10,44 @@ class Player():
         self.grounded = False
         self.mouse_pressed = [False, False, False]
 
+        # self.input_block = False
+        # self.input_timer = 0
+
     def input(self):
         keys = pygame.key.get_pressed()
+        # if not self.input_block:
+        #     if keys[pygame.K_a]:
+        #         if self.speed[0] > -60: self.speed[0] -= 4
+        #     if keys[pygame.K_d]:
+        #         if self.speed[0] < 60: self.speed[0] += 4
+        # else:
+        #     self.input_timer +=1
+        #     if self.input_timer >= 30:
+        #         self.input_block = False
 
         if keys[pygame.K_a]:
-            if self.speed[0] > -60: self.speed[0] -= 4
+            if self.speed[0] > -60:
+                self.speed[0] -= 4
         if keys[pygame.K_d]:
-            if self.speed[0] < 60: self.speed[0] += 4
-        if keys[pygame.K_SPACE] and self.grounded:
-            self.speed[1] -= 90
-            self.grounded = False
+            if self.speed[0] < 60:
+                self.speed[0] += 4
 
-        if self.mouse_pressed != pygame.mouse.get_pressed():
-            self.mouse_pressed = pygame.mouse.get_pressed()
+        if keys[pygame.K_SPACE] and self.grounded:
+            self.speed[1] = -90
+
+        if self.mouse_pressed[0] != pygame.mouse.get_pressed()[0]:
+            self.mouse_pressed[0] = pygame.mouse.get_pressed()[0]
             
             if self.mouse_pressed[0]:
-                    self.speed[0] = self.speed[0] * -1
+                self.speed[0] = self.speed[0] * -1.5
+                
+                # self.input_block = True
+        if self.mouse_pressed[2] != pygame.mouse.get_pressed()[2]:
+            self.mouse_pressed[2] = pygame.mouse.get_pressed()[2]            
+            
             if self.mouse_pressed[2]:
-                self.speed[1] = self.speed[1] * -1
-        else: pass
+                self.speed[1] = self.speed[1] * -1.5
+            else: pass
 
     def apply_mov(self, blocks):
         # left & right
@@ -42,16 +61,23 @@ class Player():
                 elif self.speed[0] < 0: 
                     self.rect.left = blocks[i].rect.right
                     self.speed[0] += 8
-
-        if self.speed[0] > 0: self.speed[0] -= 2
-        elif self.speed[0] < 0: self.speed[0] += 2
+        
+        if self.grounded: num = 2
+        else: num =1
+        if self.speed[0] > 0:
+            if self.speed[0] - num <0: self.speed[0] = 0
+            else:self.speed[0] -= num
+        
+        elif self.speed[0] < 0:
+            if self.speed[0] + num >0: self.speed[0] = 0
+            else:self.speed[0] += num
 
         # gravity
 
-        if not self.grounded:
-            if self.speed[1] <= 300: self.speed[1] += 3
-            self.rect.y += self.speed[1]
+        if self.speed[1] <= 200: self.speed[1] += 3
+        self.rect.y += self.speed[1]
 
+        self.grounded = False
         for i in blocks:
             if self.rect.colliderect(blocks[i].rect):
                 if self.speed[1] > 0: 
