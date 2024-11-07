8552b346fb7a145aaf6cc1c59596aa51d95b8732
torbin2 <torbinnet@gmail.com>
2024-11-07 18:20:35 +0100

    physics cahnges

--- player.py
+++ player.py
@@ -9,13 +9,15 @@ class Player():
         self.speed = [0,0]
         self.screen = screen
 
-        self.ground_timer = 0 #cyote_time
         self.mouse_pressed = [False, False, False]
+        self.keys_pressed = {"space" : False,
+                             "shift": False}
 
         self.gravity = 1 #1 is down, -1 is up
 
         self.state = "grounded"
-
+        self.jumps = 2
+        self.flips = 1
 
         # self.input_block = False
         # self.input_timer = 0
@@ -38,23 +40,34 @@ class Player():
             if self.speed[0] < 10:
                 self.speed[0] += 1.9
 
-        if self.state == "grounded":
-            self.ground_timer = 7 #cyote_time
-        
-        if keys[pygame.K_SPACE] and self.ground_timer > 0:
+        if keys[pygame.K_SPACE] and self.jumps > 0:
                 
-                if self.gravity == 1:
-                    self.speed[1] = -12
-                else: self.speed[1] = +12
-                self.ground_timer = 0    
-        self.ground_timer -= 1
+                if not self.keys_pressed["space"]:
+                    self.jumps -= 1
 
+                    if self.gravity == 1:
+                        self.speed[1] = -12
+                    else: self.speed[1] = +12
+                
+                self.keys_pressed["space"] = True 
+        else: self.keys_pressed["space"] = False
+
+        if keys[pygame.K_LSHIFT] and self.flips > 0:
+            
+            if not self.keys_pressed["shift"]:
+
+                self.flips -= 1
+                self.gravity = -self.gravity
+            
+            self.keys_pressed["shift"] = True
+        else: self.keys_pressed["shift"] = False
 
         if self.mouse_pressed[0] != pygame.mouse.get_pressed()[0] :
             self.mouse_pressed[0] = pygame.mouse.get_pressed()[0]
             
             if self.mouse_pressed[0] :
                 self.gravity = -self.gravity
+        
 
 
     def apply_mov(self, blocks):
@@ -73,9 +86,12 @@ class Player():
                     self.speed[0] += 4
                     if self.speed[0] > 0: self.speed[0] = 0#in case of direction shift by collision
         
+        
+        
         #friction 
         if self.state == "grounded": num = 1.5
-        else: num =1.4
+        else: num =1
+        
         if self.speed[0] > 0:
             if self.speed[0] - num <0: self.speed[0] = 0
             else:self.speed[0] -= num
@@ -85,13 +101,14 @@ class Player():
             else:self.speed[0] += num
 
         # gravity
-
-        if abs(self.speed[1]) <= 20:
+        if abs(self.speed[1]) <= 15:
             self.speed[1] += 1 * self.gravity
+        else: self.speed[1] = 15 * self.gravity #change this
+        
               
         self.rect.y += self.speed[1] * self.scale
 
-        self.state = "air"
+        colided = False
         for i in blocks:
             if self.rect.colliderect(blocks[i].rect):
                 
@@ -100,8 +117,12 @@ class Player():
                 elif self.speed[1] < 0: 
                     self.rect.top = blocks[i].rect.bottom
 
-                self.speed[1] = 0
-                self.state = "grounded"
+                colided = True
+        if colided:
+            self.state = "grounded"
+            self.speed[1] = 0
+            self.flips = 1
+            self.jumps = 2
             
     def update_settings(self, scale):
         old_center = self.rect.center # / scale 
