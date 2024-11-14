d4edbe6e4456056ea22011d01cfabca95bec5bda
Tobias H. <ytaitaro@gmail.com>
2024-11-14 11:02:19 +0100

    cleanup

--- blocks.py
+++ blocks.py
@@ -1,14 +1,18 @@
 import pygame
+import enum
+
+
+class BlockType(enum.Enum):
+    BLOCK = enum.auto()
 
 class Blocks:
-    
 
-    def __init__(self, screen, scale):
-        
-        global sc
-        sc = scale
-        
+    def __init__(self, screen, scale_):
+
         self.screen = screen
+
+        global scale
+        scale = scale_
 
         self.blocks = {}
         # for x in range(12):
@@ -23,22 +27,23 @@ class Blocks:
             self.blocks[x + 180, 0] = Block(x + 180, 0)
             self.blocks[0, x] = Block(0, x)
             self.blocks[x, 26] = Block(x, 26)
-        
+
         self.blocks[20, 15] = Block(20, 15, color="red")
         self.blocks[21, 15] = Block(21, 15, color="green")
     
     def render(self, camera):
 
-        onscreen_blocks = [key for key in self.blocks if -1 < key[0]+ (camera[0]/(10*sc)) < 49 and -1 < key[1] +(camera[1]/(10*sc)) < 28]
+        onscreen_blocks = [key for key in self.blocks
+                           if -1 < key[0] + (camera[0] / (10 * scale)) < 49
+                           and -1 < key[1] + (camera[1] / (10 * scale)) < 28]
 
         for block in onscreen_blocks:
             self.blocks[block].render(self.screen, camera)
 
-    def update_settings(self, scale):
+    def update_settings(self):
 
-        global sc
-        sc = scale
         new_blocks = {}
+
         for i in self.blocks:
             new_blocks[i] = Block(i[0], i[1])
         
@@ -46,9 +51,18 @@ class Blocks:
 
 
 class Block:
-    def __init__(self, x, y, type="block", color=("grey")):
-        self.rect = pygame.Rect(x *10*sc, y *10*sc, 10 *sc, 10 *sc)
-        self.type = type
+
+    def __init__(self,
+                 x,
+                 y,
+                 type_=BlockType.BLOCK,
+                 color="grey"):
+
+        self.rect = pygame.Rect(x * 10 * scale,
+                                y * 10 * scale,
+                                    10 * scale,
+                                    10 * scale)
+        self.type_ = type_
         self.color = color
 
     def render(self, screen, camera):
